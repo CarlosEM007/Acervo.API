d0aa2db5292d60122bf0bc0d5c6e6e2c34b77f75
carlos.menegassi <carlos.menegassi@aliare.co>
2026-05-06 16:50:16 -0300

    feat: Arrumando service.

---
+++ Acervo.API/Controllers/BookController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Acervo.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookController: ControllerBase
+    {
+        //private BookService _service
+    }
+}
---
+++ Acervo.Application/Service/BookService.cs
@@ -0,0 +1,46 @@
+﻿using Acervo.Domain.Common;
+using Acervo.Domain.Entities;
+using Acervo.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acervo.Application.Service
+{
+    public class BookService
+    {
+        private IBookRepository _repository;
+
+        public BookService(IBookRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Result<Book>> GetById(long Id)
+        {
+            return await _repository.GetById(Id);
+        }
+
+        public async Task<Result<List<Book>>> GetAllBook()
+        {
+            return await _repository.GetAllBook();
+        }
+
+        public Result Delete(Book Entity)
+        {
+            return _repository.Delete(Entity);
+        }
+
+        public async Task<Result> Insert(Book Entity)
+        {
+            return await _repository.Insert(Entity);
+        }
+
+        public async Task<Result> Update(Book Entity)
+        {
+            return await _repository.Update(Entity);
+        }
+    }
+}
