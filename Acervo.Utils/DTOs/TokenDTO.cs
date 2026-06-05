36677352903a1b864a98e58c096012e29b54fcf3
Carlos <carloseduardomenegassi0@gmail.com>
2026-06-05 20:09:10 -0300

    feat: Pequena reestruturação, para que a Web tenha acesso ao Utils.

---
+++ Acervo.Utils/DTOs/TokenDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace Acervo.Application.DTOs
+{
+    public class TokenDTO
+    {
+        public string Token { get; set; }
+        public DateTime Expiracao { get; set; }
+
+        public TokenDTO(string token, DateTime expiracao)
+        {
+            Token = token;
+            Expiracao = expiracao;
+        }
+    }
+}
---
+++ Acervo.Web/UserService.cs
@@ -0,0 +1,19 @@
+﻿using Acervo.Application.DTOs;
+
+namespace Acervo.Web
+{
+    public class UserService
+    {
+        private readonly HttpClient _httpClient;
+
+        public UserService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<bool> CreateUser(LoginDto login)
+        {
+
+        }
+    }
+}
